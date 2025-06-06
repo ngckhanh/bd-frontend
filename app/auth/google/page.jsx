96f04d94ee3bb25b8d35a8e02997b8061a95ba36
ngckhanh <ngockhanh7643@gmail.com>
2025-06-06 22:41:25 +0700

    update

--- app/auth/google/page.jsx
+++ app/auth/google/page.jsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation"
 
 export default function GoogleOauth() {
-    return redirect('https://accounts.google.com/o/oauth2/auth?client_id=824131161854-3hh6urnd61oqvgihf96hosl908k4932u.apps.googleusercontent.com&redirect_uri=https://api.bachduong.app/auth/google&response_code=code&scope=email openid profile&access_type=offline&prompt=consent&response_type=code&include_granted_scopes=true')
+    return redirect('https://accounts.google.com/o/oauth2/auth?client_id=613079491877-2okfkc757h5ea0njpm6b7s7anec815la.apps.googleusercontent.com&redirect_uri=https://api.bachduong.app/auth/google&response_type=code&scope=email%20openid%20profile&access_type=offline&prompt=consent&include_granted_scopes=true')
 }
